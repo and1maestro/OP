a3d66d858126e2a6090ed58c46702acb4480b380
and1maestro <90398432+and1maestro@users.noreply.github.com>
2021-09-21 14:50:31 +0300

    Add files via upload

---
+++ Lab C1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+#include <math.h>
+
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+    double a, b;
+    cout << "Введiть кут: ";
+    cin >> a;
+    b = a * 3.1415926535 / 180;
+    cout << b;
+    return 0;
+}
---
+++ _19Var.py
@@ -0,0 +1,9 @@
+import math
+
+print("Введіть градусну міру кута")
+x = float(input("x = "))
+
+rad = x * math.pi / 180
+
+print("Радінна міра кута = ", rad)
+
