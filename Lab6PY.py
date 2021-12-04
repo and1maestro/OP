336a829d2d1ba0ade801bc5b627659496100166c
and1maestro <90398432+and1maestro@users.noreply.github.com>
2021-12-04 16:38:51 +0200

    Update Lab6PY.py

--- Lab6PY.py
+++ Lab6PY.py
@@ -1,5 +1,3 @@
-# -- coding: cp1251 --
-
 def sum(n):
     sd = 0
     for i in range(1, n + 1):
@@ -12,4 +10,4 @@ n = int(input("n = "))
 for x in range(2, n):
     y = sum(x) - x
     if (sum(y)-y) == x and (sum(x)-x) == y and x != y and (y - x) > 0:
-        print(x, "i", y, "- дружні числа")
+        print(x, "i", y, "- Г¤Г°ГіГ¦Г­Ві Г·ГЁГ±Г«Г ")
