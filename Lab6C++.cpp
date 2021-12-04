4655a7586898626efe15b524a0dfc23e29cce213
and1maestro <90398432+and1maestro@users.noreply.github.com>
2021-12-04 16:38:16 +0200

    Add files via upload

---
+++ Lab6C++.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int sum(int n)
+{
+    int sd = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+            sd += i;
+    }
+    return sd;
+}
+
+void friendlyNumbers(int n)
+{
+    int x = 2;
+    for (x; x < n; x++)
+    {
+        int y;
+        y = sum(x) - x;
+        if ((sum(x) - x) == y && (sum(y) - y) == x && x != y && (y - x) > 0)
+            cout << x << " and " << y << " are friendly numbers";
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "n = ";
+    cin >> n;
+    friendlyNumbers(n);
+
+    system("pause");
+}
---
+++ Lab6PY.py
@@ -0,0 +1,15 @@
+# -- coding: cp1251 --
+
+def sum(n):
+    sd = 0
+    for i in range(1, n + 1):
+        if (n % i == 0): 
+            sd += i
+    return(sd)
+
+n = int(input("n = "))
+
+for x in range(2, n):
+    y = sum(x) - x
+    if (sum(y)-y) == x and (sum(x)-x) == y and x != y and (y - x) > 0:
+        print(x, "i", y, "- дружні числа")
