77c461fd091246e8da47b61c1ff6bca8845a6dc1
and1maestro <90398432+and1maestro@users.noreply.github.com>
2021-12-25 10:52:12 +0200

    Add files via upload

---
+++ Lab9C++.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int str(int a)
+{
+    char* intStr = itoa(a);
+    string str = string(intStr);
+}
+
+int freq(string a, string b)
+{
+    int n = 0;
+    int len = b.length();
+    for (size_t i = 0; i < len; i++)
+    {
+        if (b[i] == a)
+        {
+            n++;
+        }
+    }
+}
+
+int minmax(string text)
+{
+    string CharNum = "";
+    int minn = text.length();
+    int maxx = 0;
+    string min = "";
+    string max = "";
+    for (int i = 0; i < text.length(); i++)
+    {
+        if (find(begin(text), end(text), i) != end(text))
+            if (freq(i,text) > maxx)
+            {
+                maxx = freq(i, text);
+                max = str(i);
+            }
+            else if (freq(i, text) == maxx)
+            {
+                max = max + " " + str(i);
+            }
+            if (freq(i, text) < minn)
+            {
+                minn = freq(i, text);
+                min = str(i);
+            }
+            else if (freq(i, text) == minn)
+            {
+                min = min + " " + str(i);
+                CharNum = CharNum + i;
+            }
+    }
+    string result = "Max: " + max + " " + str(maxx) + " time(s)" + "\n" + "Min: " + min + " " + str(minn) + " time(s)";
+    return result
+}
+
+int main()
+{
+    string your_text;
+    cout << "Enter your text: ";
+    cin >> your_text;
+    cout << your_text;
+    string res = minmax(your_text);
+    cout << res;
+}
